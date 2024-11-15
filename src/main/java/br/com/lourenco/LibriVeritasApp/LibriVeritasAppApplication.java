e4a506440b561ba177ddad7b2a6353f45b9329f6
Luiz Lourenco <luiz.lourenco@dhl.com>
2024-11-15 19:02:07 -0300

    Comint inicial

---
+++ src/main/java/br/com/lourenco/LibriVeritasApp/LibriVeritasAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.lourenco.LibriVeritasApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibriVeritasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibriVeritasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lourenco/LibriVeritasApp/LibriVeritasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lourenco.LibriVeritasApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibriVeritasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
