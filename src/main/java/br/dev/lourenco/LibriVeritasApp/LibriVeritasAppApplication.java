fa0b427520300c6cf36db204fd345e6fdc696316
Luiz Lourenco <luiz.lourenco@dhl.com>
2024-11-17 14:02:02 -0300

    Primeiro commit

---
+++ src/main/java/br/dev/lourenco/LibriVeritasApp/LibriVeritasAppApplication.java
@@ -0,0 +1,13 @@
+package br.dev.lourenco.LibriVeritasApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibriVeritasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibriVeritasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/lourenco/LibriVeritasApp/LibriVeritasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.lourenco.LibriVeritasApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibriVeritasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
